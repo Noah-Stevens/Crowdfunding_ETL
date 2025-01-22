00a27bf6c195126f7997ba574ee607642def3384
Noah-Stevens <Noahstevens111@gmail.com>
2025-01-21 21:34:00 -0500

    Prep For Penultimate Pull

--- Starter_Files/Part 2/schema.sql
+++ Starter_Files/Part 2/schema.sql
@@ -9,7 +9,7 @@
 CREATE TABLE "category" (
     "category_id" varchar(10)   NOT NULL,
     "category" varchar(50)   NOT NULL,
-    "last_updated" timestamp   NOT NULL,
+    "last_update" timestamp default localtimestamp  NOT NULL,
     CONSTRAINT "pk_category" PRIMARY KEY (
         "category_id"
      )
@@ -18,7 +18,7 @@ CREATE TABLE "category" (
 CREATE TABLE "subcategory" (
     "subcategory_id" varchar(10)   NOT NULL,
     "subcategory" varchar(50)   NOT NULL,
-    "last_updated" timestamp   NOT NULL,
+    "last_update" timestamp default localtimestamp  NOT NULL,
     CONSTRAINT "pk_subcategory" PRIMARY KEY (
         "subcategory_id"
      )
@@ -39,7 +39,7 @@ CREATE TABLE "campaign" (
     "end_date" date   NOT NULL,
     "category_id" varchar(10)   NOT NULL,
     "subcategory_id" varchar(10)   NOT NULL,
-    "last_updated" timestamp   NOT NULL
+    "last_update" timestamp default localtimestamp  NOT NULL
 );
 
 CREATE TABLE "contacts" (
@@ -47,7 +47,7 @@ CREATE TABLE "contacts" (
     "first_name" varchar(50)   NOT NULL,
     "last_name" varchar(50)   NOT NULL,
     "email" varchar(100)   NOT NULL,
-    "last_updated" timestamp   NOT NULL,
+    "last_update" timestamp default localtimestamp  NOT NULL,
     CONSTRAINT "pk_contacts" PRIMARY KEY (
         "contact_id"
      )
